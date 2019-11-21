0b66abdae905d08c0f7eecdae9b9f8bffd671365
benjose22 <benjose22@gmail.com>
2019-11-21 13:34:39 +0530

    titles and descriptions of allure reports

--- libraries/library_add_remove_elements.py
+++ libraries/library_add_remove_elements.py
@@ -13,12 +13,14 @@ class AddRemoveActions(object):
         self.driver.find_element_by_xpath(Locators.xpath_add_element_button).click()
 
     @allure.step
-    @allure.feature("count_delete_buttons")
     def count_delete_buttons(self):
         elements = self.driver.find_elements_by_xpath(Locators.xpath_delete_button_list)
         return elements
 
-    @allure.story("click_delete_button")
     def click_delete_button(self):
         delete_button = self.driver.find_element_by_xpath(Locators.xpath_first_delete_button)
         delete_button.click()
+
+    @allure.step
+    def to_delete(self, param1, param2 = 2):
+        pass
--- tests/test_add_remove_elements.py
+++ tests/test_add_remove_elements.py
@@ -1,8 +1,16 @@
 import pytest
+import allure
 from libraries.library_add_remove_elements import AddRemoveActions
 
 
 # Just confirm the functionality works
+@allure.title("This test has a custom title")
+@allure.description("""
+Multiline test description.
+That comes from the allure.description decorator.
+
+Nothing special about it.
+""")
 @pytest.mark.high_priority
 @pytest.mark.sanity_test
 def test_add_button(open_browser):
@@ -11,6 +19,7 @@ def test_add_button(open_browser):
     class_add_remove = AddRemoveActions(driver, url)
     class_add_remove.click_on_add_element()
     buttons = class_add_remove.count_delete_buttons()
+    class_add_remove.to_delete(3)
     assert len(buttons) == 1
 
 
