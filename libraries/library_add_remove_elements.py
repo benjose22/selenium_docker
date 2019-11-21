2d19c69c62abf35ed57e946657917f8d7ab808bd
benjose22 <benjose22@gmail.com>
2019-11-21 10:46:51 +0530

    titles of allure reports

--- libraries/library_add_remove_elements.py
+++ libraries/library_add_remove_elements.py
@@ -1,4 +1,5 @@
 from libraries.locators import Locators
+import allure
 
 
 class AddRemoveActions(object):
@@ -7,13 +8,16 @@ class AddRemoveActions(object):
         self.driver = driver
         driver.get(url)
 
+    @allure.title("click_on_add_element")
     def click_on_add_element(self):
         self.driver.find_element_by_xpath(Locators.xpath_add_element_button).click()
 
+    @allure.feature("count_delete_buttons")
     def count_delete_buttons(self):
         elements = self.driver.find_elements_by_xpath(Locators.xpath_delete_button_list)
         return elements
 
+    @allure.story("click_delete_button")
     def click_delete_button(self):
         delete_button = self.driver.find_element_by_xpath(Locators.xpath_first_delete_button)
         delete_button.click()
