d1806e1c47c9ec899d1623ca90dfcc8569ae6c49
benjose22 <benjose22@gmail.com>
2019-12-11 09:45:44 +0530

    another temporary change

--- conftest.py
+++ conftest.py
@@ -27,7 +27,7 @@ def open_browser(request):
         if executor == "remote":
             caps = {'browserName': os.getenv('BROWSER', 'chrome')}
             driver = webdriver.Remote(
-                command_executor='http://localhost:4444/wd/hub',
+                command_executor='http://selenium-hub:4444/wd/hub',
                 desired_capabilities=caps)
         else:
             driver = webdriver.Chrome()
